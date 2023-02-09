1799b09590ca5928c8fb062b96b24f8ef5651a1a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-02-09 01:06:48 +0000

    bools, conditionals and switch

--- src/examples/03_conditionals/01_if/if.cpp
+++ src/examples/03_conditionals/01_if/if.cpp
@@ -17,7 +17,21 @@ bool get_notted(bool b1)
     return !(b1);
 }
 
+bool get_bool_order_of_ops(bool b1, bool b2, bool b3)
+{
+    return !b1 || b2 && b3; //TODO validate order of prec
+}
+
 //write code for function named overtime that accepts a double hours and returns true if 
 //hours over 40, false otherwise
+bool get_is_over_time(int hours)
+{
+    return hours > 40; //T or F -> boolean expression
+}
+
+bool is_even(int num)
+{
+    return num % 2 == 0;
+}
 
 
