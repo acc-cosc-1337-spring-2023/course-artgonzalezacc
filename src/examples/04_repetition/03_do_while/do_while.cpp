d59e6101cea926f38b1abcd48b0853e4e0d05269
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-02-14 01:24:39 +0000

    while, for, and do while

--- src/examples/04_repetition/03_do_while/do_while.cpp
+++ src/examples/04_repetition/03_do_while/do_while.cpp
@@ -1,7 +1,20 @@
 #include "do_while.h"
 
+using std::cin; using std::cout;
+
 //Write code for void function prompt_user to loop until
-//user opts not to continue.  
+//user opts not to continue. 
+void prompt_user()
+{
+    auto choice = 'n';
+
+    do
+    {
+        cout<<"Enter y to continue, any other key to exit ?";
+        cin>>choice;
+    }
+    while(choice == 'y' || choice == 'Y');
+} 
 
 
 
