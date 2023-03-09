65aa40cf83a42385886aae83b1315ef031f03ded
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-03-09 00:53:30 +0000

    modify atm to use vector of bankaccount pointers

--- src/examples/08_classes/01_bank/atm.cpp
+++ src/examples/08_classes/01_bank/atm.cpp
@@ -12,9 +12,15 @@ void display_menu()
     cout<<"4-Exit\n";
 }
 
-void run_menu(BankAccount& account)
+void run_menu(std::vector<BankAccount*> &accounts)
 {
     auto option = 0;
+    auto choice = 0;
+
+    cout<<"Checking(1) or savings(2)?";
+    cin>>choice;
+
+    BankAccount* account = accounts[choice - 1];
 
     do
     {
@@ -26,7 +32,7 @@ void run_menu(BankAccount& account)
     while(option != 4);
 }
 
-void handle_menu_option(int option, BankAccount& account)
+void handle_menu_option(int option, BankAccount* account)
 {
     auto amount = 0;
 
@@ -35,16 +41,16 @@ void handle_menu_option(int option, BankAccount& account)
     case 1:
         cout<<"Enter deposit amount: ";
         cin>>amount;
-        account.deposit(amount);
+        account->deposit(amount);
         break;
     case 2:
         cout<<"Enter withdraw amount: ";
         cin>>amount;
-        account.withdraw(amount);
+        account->withdraw(amount);
         break;
     case 3:
         cout<<"Balance: ";
-        cout<<account.get_balance();
+        cout<<account->get_balance();
         break;
     case 4:
         cout<<"Exiting...\n";
--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,48 +1,22 @@
 #include<iostream>
 #include<time.h>
+#include<vector>
 #include "checking_account.h"
 #include "atm.h"
 #include "savings_account.h"
 
-using std::cout; using std::cin;
+using std::cout; using std::cin; using std::vector;
 
 int main()
 {
-	BankAccount* bank_account;//doesn't point to a variable
-
 	srand(time(NULL));//generate true randoms on each main run execution
+	vector<BankAccount*> accounts;
 	SavingsAccount savings;
-	show_balance(savings);
-	cout<<savings.get_balance()<<"\n";
+	CheckingAccount checking;
+	accounts.push_back(&checking);
+	accounts.push_back(&savings);
 
-	bank_account = &savings;
-	cout<<bank_account->get_balance()<<"\n";//must use arrow -> operator
-
-	CheckingAccount account;//cust 1
-
-
-	/*
-	auto choice = 's';
-
-	cout<<"Use checking or savings?";
-	cin>>choice;
-
-	if(choice == 'c')
-	{
-		cout<<account;
-		cout<<account.get_balance()<<"\n";
-		show_balance(account);//use the friend free function
-		run_menu(account);
-	}
-	else
-	{
-		cout<<savings;
-		cout<<savings.get_balance()<<"\n";
-		show_balance(savings);//use the friend free function
-		run_menu(savings);
-	}
-
-	cout<<account.get_balance()<<"\n";*/
+	run_menu(accounts);
 
 	return 0;
 }
