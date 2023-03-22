d591d39f9965f2f19ee0f98c39f669581fd731cb
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-03-22 23:32:59 +0000

    stack shapes w pointers

--- src/examples/08_classes/02_shapes/main.cpp
+++ src/examples/08_classes/02_shapes/main.cpp
@@ -13,6 +13,21 @@ iterate with auto
 
 int main() 
 {
+	Shape shape;
+	shape.draw();
+
+	Line line;
+	line.draw();
+
+	Circle circle;
+	circle.draw();
+
+	Shape* shape_ptr;
+	shape_ptr = &line;
+	shape_ptr->draw();
+
+	shape_ptr = &circle;
+	shape_ptr->draw();
 
 	return 0;
 }
--- src/examples/08_classes/02_shapes/shape.h
+++ src/examples/08_classes/02_shapes/shape.h
@@ -1,1 +1,13 @@
+#include<iostream>
+
 //shape.h
+#ifndef SHAPE_H
+#define SHAPE_H
+
+class Shape
+{
+public:
+    virtual void draw();    
+};
+
+#endif
