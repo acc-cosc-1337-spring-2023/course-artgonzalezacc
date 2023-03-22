eb67c20dca27521a1316996c209d23369029c212
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-03-22 23:11:13 +0000

    atm w unique pointer

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<memory>
 #include<time.h>
 #include<vector>
 #include "checking_account.h"
@@ -6,15 +7,20 @@
 #include "savings_account.h"
 
 using std::cout; using std::cin; using std::vector;
+using std::unique_ptr; using std::make_unique;
+using std::move;
 
 int main()
 {
 	srand(time(NULL));//generate true randoms on each main run execution
-	vector<BankAccount*> accounts;
-	SavingsAccount savings;
-	CheckingAccount checking;
-	accounts.push_back(&checking);
-	accounts.push_back(&savings);
+	vector<unique_ptr<BankAccount>> accounts;//empty list
+	unique_ptr<BankAccount> savings = make_unique<SavingsAccount>();
+	unique_ptr<BankAccount> checking = make_unique<CheckingAccount>();
+	accounts.push_back(move(checking));
+	accounts.push_back(move(savings));
+
+	cout<<accounts[0]->get_balance()<<"\n";
+	cout<<accounts[1]->get_balance()<<"\n";
 
 	run_menu(accounts);
 
