b1150b95d66eba438322e4e3fe956fe56a9a68d7
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-04-03 23:31:11 +0000

    Create ATMData n use in main

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 #include "atm.h"
 #include "customer.h"
+#include "atm_data.h"
+#include<iostream>
 
 using std::vector;
 
@@ -16,7 +18,17 @@ int main()
 	customers.push_back(Customer());
     customers.push_back(Customer());
 
-	run_menu(customers);
+	ATMData data;
+	data.save_customers(customers);
+	std::vector<Customer>& cus = data.get_customers();
+
+	for(auto& c: cus)
+	{
+		std::cout<<c.get_account(0)->get_balance()<<" ";
+		std::cout<<c.get_account(1)->get_balance()<<"\n";
+	}
+
+	//run_menu(customers);
 
 	return 0;
 }
